28221c4f023fa5657c6aee9070c3bd11545b602b
lua-oliv7 <luana.a.oliveira7@hotmail.com>
2026-08-27 13:14:23 -0300

    Login/index/app e Middlewire

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,10 +2,45 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Login extends Component
 {
+    public $email = '';
+    public $password = '';
+    public $remember = false;
+
+    public function login(){
+        $credentials = $this->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ], [
+            'email.required' => 'O e-mail é obrigatório',
+            'email.email' => 'Formato de e-mail incorreto',
+            'password.required' => 'Senha obrigatória'
+        ]);
+
+        if(!Auth::attempt($credentials, $this->remember)){
+            session()->flash('error', 'E-mail e senha inválidos');
+        }
+
+        $user = Auth::user();
+
+        if(!$user->isAdmin()){
+            Auth::logout();
+
+            request()->session()->invalidate();
+            request()->session()->regenerateToken();
+
+            session()->flash('error', 'Não autorizado');
+        }
+
+        request()->session()->regenerate();
+
+        return redirect()->route('dashboard');
+    }
+
     public function render()
     {
         return view('livewire.auth.login');
